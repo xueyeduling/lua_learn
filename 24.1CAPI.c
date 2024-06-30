720f79928cb24579aa873ffc9de2d5a98ee99f8b
xueyeduling <xueyeduling@outlook.com>
2024-07-01 00:14:22 +0800

    24 chapter

--- 24.1CAPI.c
+++ 24.1CAPI.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <lua.h>
 #include <lauxlib.h>
 #include <lualib.h>
@@ -6,7 +7,7 @@ int main(void)
 {
     char buff[256];
     int error;
-    lua_State *L = lua_open(); /* opens Lua */
+    lua_State *L = luaL_newstate(); /* opens Lua */
     luaopen_base(L);           /* opens the basic library */
     luaopen_table(L);          /* opens the table library */
     luaopen_io(L);             /* opens the I/O library */
